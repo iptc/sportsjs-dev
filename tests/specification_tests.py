9cb0e34a69594bf6630acfcd1db6a2358a6a10ff
bquinn <brendan@cluefulmedia.com>
2018-07-11 14:54:16 +0300

    Updated to new version of schema with lots of additionalProperties set.
    This means that we can actually test more of the example files, which turned up
    lots of errors in the examples which have now been fixed.
    
    Some elements can't be augmented using additionalProperties, hopefully
    new constraints in json schema draft-08 will allow for these to be tested
    properly.

--- tests/specification_tests.py
+++ tests/specification_tests.py
@@ -39,8 +39,12 @@ class TestSportsJSSchema(unittest.TestCase):
     sportsjs_schema = None
 
     def __init__(self, *args, **kwargs):
-        # if we put this in setUp() it would load the schema for each
-        # test which is unnecessary.
+        """
+        Set up paths and load the schema.
+
+        If we put this in setUp() rather than __init__(), it would
+        load the schema for each test which is unnecessary.
+        """
         import os
         self.current_path = os.path.dirname(os.path.realpath(__file__))
         schema_filename = os.path.join(
@@ -60,67 +64,6 @@ class TestSportsJSSchema(unittest.TestCase):
         """
         self.assertIsNone(jsonschema.validate({}, self.sportsjs_schema))
 
-    """
-    def test_garbage_fails(self):
-        with self.assertRaises(jsonschema.exceptions.ValidationError):
-            jsonschema.validate(
-                {
-                    "foo": "bar"
-                },
-                self.sportsjs_schema
-            )
-
-    def test_generic_alert(self):
-        # examples/generic-alert.json
-        instance = json.loads(
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_simple_soccer_example(self):
-        instance = json.loads(
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def load_example_file(self, filename):
-        filename = os.path.join(
-                        self.current_path,
-                        '..',
-                        'examples',
-                        filename
-                    )
-        with open(filename, 'r') as jsonfile:
-            instance = json.load(jsonfile)
-        return instance
-
-    def test_example_file_1(self):
-        instance = self.load_example_file('generic-alert.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-    
-    def test_example_file_2(self):
-        instance = self.load_example_file('americanFootballEventSummary.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_example_file_3(self):
-        instance = self.load_example_file('biathlon_mixedrelay_g2.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_example_file_4(self):
-        instance = self.load_example_file('golf-tour-for-sportsjs.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_example_file_5(self):
-        instance = self.load_example_file('skiing_vasaloppet_2015_g2.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_example_file_6(self):
-        # this example is supposed to fail
-        with self.assertRaises(jsonschema.exceptions.ValidationError):
-            instance = self.load_example_file('soccer-simple-sample-pre-game-not-to-validate.json')
-            self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-
-    def test_example_file_7(self):
-        instance = self.load_example_file('soccer-simple-sample-pre-game.json')
-        self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
-    """
-
     def get_files_in_folder(self, folder_name):
         folder_name = os.path.join(
                         self.current_path,
@@ -133,7 +76,13 @@ class TestSportsJSSchema(unittest.TestCase):
             instance = json.load(jsonfile)
         return instance
 
-    def test_all_passing_example_files(self):
+    def test_all_passing_unit_test_files(self):
+        """
+        Run all files in TEST_FILES_FOLDER/should_pass against the schema.
+        They should all pass (ie they are all valid against the schema).
+
+        We use "subTest" so we can see which file failed in test output.
+        """
         test_files_path = os.path.join(self.current_path, TEST_FILES_FOLDER, 'should_pass')
         testfiles = self.get_files_in_folder(test_files_path)
         for file in testfiles:
@@ -141,12 +90,20 @@ class TestSportsJSSchema(unittest.TestCase):
                 instance = self.load_test_file(test_files_path, file)
                 self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
 
-    def test_all_failing_example_files(self):
+    def test_all_failing_unit_test_files(self):
+        """
+        Run all files in TEST_FILES_FOLDER/should_fail against the schema.
+        They should all fail (ie they are all invalid in some way).
+
+        We use "subTest" so we can see which file failed in test output.
+        """
         test_files_path = os.path.join(self.current_path, TEST_FILES_FOLDER, 'should_fail')
         testfiles = self.get_files_in_folder(test_files_path)
         for file in testfiles:
             with self.subTest(file=file):
-                with self.assertRaises(jsonschema.exceptions.ValidationError) or self.assertRaises(json.decoder.JSONDecodeError):
+                with (
+                    self.assertRaises(jsonschema.exceptions.ValidationError)
+                    or self.assertRaises(json.decoder.JSONDecodeError)):
                     instance = self.load_test_file(test_files_path, file)
                     self.assertIsNone(jsonschema.validate(instance, self.sportsjs_schema))
 
